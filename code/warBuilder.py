cd8aa4a38d70fedb62aa130e1b8163938b166939
CleanestMink126 <gns126@gmail.com>
2017-12-04 23:42:01 -0500

    added better reseource allocation

--- code/warBuilder.py
+++ code/warBuilder.py
@@ -16,17 +16,20 @@ class Actor:
     '''Actor class that contains the ID, capital position and dictionaries of its
     provinces and borders'''
     borderStates = {} #k = stateID v = set of position representing borders
-    borderStateRes = {} #k = stateID v = total res
+    borderStateRes = {} #k = stateID v = total res that each enemey has put against this actor
+    borderStateSelf = {} #k = stateID v = total res that this actor has put against each enemy k
+    borderStateAttackProb = {} #k = stateID, v = attackProbability of this actor against state k
+
     warStates = {} #k = state we are at war with v = total damage
-    totalEnemy = 0
-    totalWar = 0
+    totalEnemy = 0 # total enemy resources
+    totalWar = 0 # totla resources of peop (adjusted by lowest value)
     numTotalBorders = 0
     def __init__(self, actorNum, pos):
         self.actorNum = actorNum
         self.capital = pos
         self.provinces = {pos: Province(pos)} #k = pos v = province obj
         self.borders = {pos}#list of border states
-        self.probexpand =  np.random.ranf()/2 # probability to expand
+        self.probexpand =  np.random.ranf()/4 + .25 # probability to expand
         self.fixesRes = .5
 
 
@@ -51,23 +54,44 @@ class Actor:
         return provinces.pop(pos)
 
     def updateBorders(self,warObj):
-        borderList = list(self.borders)
+        self.borderStateRes = {}
+        self.borderStates = {}
+        borderList = list(self.borders)#get a list of the border states
+        totalborder = 0#total borders with a warring state found
+        self.minV = None#greatest advantage over a warring state
         for k in borderList:
-            borders,same = warObj.numBorder(self.actorNum, k)
-            self.provinces[k].numBorders = len(borders)
-            if not len(borders):
+            borders,same = warObj.numBorder(self.actorNum, k)#find the borders for each border province
+            self.provinces[k].numBorders = len(borders) #update the correstponding province
+            if not len(borders):#if no borders remove the obj
                 self.borders.remove(k)
             else:
                 for b in borders:
-                    state = warObj.npBoard[b]
-                    if state in self.borderStates:
-                        self.borderStates[state].add(k)
-                        self.borderStateRes[state] = self.borderStateRes.pop(state) + warObj.actorDict[state].provinces[b].res
+                    state = warObj.npBoard[b]#get the state
+                    if state in self.borderStates:#if state indexed
+                        self.borderStates[state].add(k)#add border
+                        self.borderStateRes[state] = self.borderStateRes.pop(state) + warObj.actorDict[state].provinces[b].res#add enemy resource
+                        if state in self.warStates:#if warring with the state
+                            totalborder += 1
+                            diffBorder = (warObj.actorDict[state].provinces[b].res - self.provinces[k].res)#find diff
+                            if (not minV or diffBorder < minV):#check if min
+                                self.minV = diffBorder
                     else:
                         self.borderStates[state] = {k}
-                        self.borderStateRes[state] = 0
+                        self.borderStateRes[state] = warObj.actorDict[state].provinces[b].res
+                        if state in self.warStates:
+                            totalborder += 1
+                            diffBorder = (warObj.actorDict[state].provinces[b].res - self.provinces[k].res)
+                            if (not minV or diffBorder < minV):
+                                self.minV = diffBorder
+                    # print(self.borderStateRes)
+        for k,v in self.borderStateRes.items():#check border value from other person in each case
+            if self.actorNum in warObj.actorDict[k].borderStateRes:
+                res = warObj.actorDict[k].borderStateRes[self.actorNum]
+                self.borderStateSelf[k] = res
+            else:
+                self.borderStateSelf[k] = self.borderStateRes[k]
         self.totalEnemy = sum(self.borderStateRes.values())
-        self.totalWar = sum([self.borderStateRes[x] for x,v in self.warStates])
+        if self.minV: self.totalWar = sum([self.borderStateRes[x] for x,v in self.warStates]) + totalborder * -1 * self.minV
 
     def distributeResources(self, totalRes,warObj):
         borderList = list(self.borders)
@@ -85,7 +109,23 @@ class Actor:
                     eRes = warObj.actorDict[state].provinces[b].res
                     self.provinces[k].res += fixedRate * eRes
                     if state in self.warStates:
-                        self.provinces[k].res += variableRate * eRes
+                        self.provinces[k].res += (variableRate -self.minV) * eRes
+
+    def updateProbAttack(self):
+        """Defines probability of Attacking an enemey state based on
+        how much I allocated against the enemy / how much the enemy has allocated against you """
+        borderStateRes = self.borderStateRes #k = stateID v = total res
+        borderStateSelf = self.borderStateSelf
+        enemyStates = list(borderStateRes.keys())
+        print("SELF",borderStateSelf)
+        print("OTHER",borderStateRes)
+        print("PARTS", self.borderStates)
+        for actor in enemyStates:
+            rate = borderStateSelf[actor] / borderStateRes[actor]
+            self.borderStateAttackProb[actor] = self.sigmoid(rate)
+
+    def sigmoid(self, rate):
+        return 1 / (1 + np.exp(3-rate))
 
 class War2D:
     """Implements Conway's Game of Life."""
@@ -111,6 +151,7 @@ class War2D:
             self.numberProvinces += 1
 
         self.image = np.zeros((boardSize, boardSize,3), np.float32)#create graphical display board
+        self.image2 = np.zeros((boardSize, boardSize,3), np.float32)
         # for i,v in enumerate(self.npBoard):
         #     for j,w in enumerate(v):
         #         self.image[i,j,:] = self.dictCols[w]
@@ -119,6 +160,22 @@ class War2D:
     def show(self):#show the current state of the board
         plt.imshow(self.image)
         plt.show()
+
+    def show2(self):#show the current state of the board
+        plt.imshow(self.image2)
+        plt.show()
+
+    def colorCodeProvinces(self):
+        maxv = 0
+        for x in range(self.boardSize):
+            for y in range(self.boardSize):
+                actor = self.actorDict[self.npBoard[x,y]]
+                province = actor.provinces[(x,y)]
+                self.image2[x,y,:] = [province.res, province.res, province.res]
+                if province.res > maxv: maxv = province.res
+
+        self.image2 /= maxv
+
     def getneighbors(self,pos):#get the neighbors of a position
         boolarr = [pos[0]>0,pos[0]<self.boardSize-1,pos[1]>0,pos[1]<self.boardSize-1]
         posarr = [(pos[0]-1,pos[1]),(pos[0]+1,pos[1]),(pos[0],pos[1]-1),(pos[0],pos[1]+1)]
@@ -149,23 +206,23 @@ class War2D:
 
     def step(self):
         """Executes one time step."""
+
         if self.initStage:
             for i in self.actorDict.values():#loop through and expand actors. should be shuffled in future
                 self.actorExpand(i)
-            if self.numberProvinces == self.totalSquares:
+            if self.numberProvinces >= self.totalSquares:
                 self.initRes()
                 self.initStage = False
         else:
+            self.colorCodeProvinces()
             for actor in self.actorDict.values():#loop through and expand actors. should be shuffled in future
-                print("UHOH")
                 actor.updateBorders(self)
                 totalRes = self.FIXED_RES * len(actor.provinces)
                 actor.distributeResources(totalRes, self)
-                print(actor.totalEnemy)
-        # if self.prepStage:
-            # pass
-        # if self.battleStage:
-            # pass
+                actor.updateProbAttack()
+                # print(actor.totalEnemy)
+                # print(actor.borderStateAttackProb)
+
 
     def actorExpand(self,actor):
         borderList = list(actor.borders)
@@ -189,7 +246,6 @@ class War2D:
         return False, None
 
     def initRes(self):
-
         actorList = list(self.actorDict.values())
         for actor in actorList:
             actor.numTotalBorders = 0
@@ -205,7 +261,10 @@ class War2D:
 
 if __name__ == "__main__":
 
-    mywar = War2D(20, 20)
-    for i in range(10):
+    mywar = War2D(100, 10)
+    for i in range(60):
         mywar.step()
-        mywar.show()
+        # if not i % 10:
+        #     mywar.show()
+    mywar.show()
+    mywar.show2()
